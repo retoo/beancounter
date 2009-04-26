f43acabdd28c755c043c244e514ba6e831fff7ed
Reto Schüttel <reto@schuettel.ch>
2009-04-26 15:28:46 +0200

    First version, values can be parsed

---
+++ beancounter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# Copyright (c) 2009 Reto Schüttel <reto -(a)- schuettel (o) ch>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Reads the servers4you / VZ Virtual Server bean statistics and creates nice RRD graphs
+
+from __future__ import with_statement
+
+import sys
+sys.path.append('/usr/local/rrdtool-1.3.7/lib/python2.5/site-packages/')
+import rrdtool
+
+import re
+
+BEAN_FILE = "example_input"
+BEAN_VERSION = "2.5"
+
+NUMERIC_LINE = re.compile("^\d+:$")
+
+with open(BEAN_FILE) as fh:
+  for line in fh:
+    parts = line.split()
+
+    if parts[0] == "Version:":
+      # Version: 2.5
+      version = parts[1]
+
+      if version != BEAN_VERSION:
+        log.critical("Unexpected bean version %s, script verified for %s, please " +
+                     "check for updates" % version, BEAN_VERSION)
+        sys.exit(1)
+
+      continue
+    if parts[0] == "uid":
+      #  uid  resource                     held              maxheld              barrier                limit              failcnt
+      # skip column line
+      continue
+    if NUMERIC_LINE.match(parts[0]):
+      # 187099: kmemsize                 10191833             13450533             16934908             18628398                    0
+      # scrap first item in uid line
+      parts.pop(0)
+
+    print parts
+    resource, held, maxheld, barrier, limit, failcnt = parts
